e1a8c991e4bfc4241c45e4c8c356b0425acd9adc
irinoirino <genkishoot@icloud.com>
2025-07-16 23:31:43 +0900

    リスト羅列

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,11 +19,53 @@ const pushList = () => {
 </script>
 
 <template>
-  <h1>TODO-APP</h1>
-  <form @submit.prevent="pushList">
-    <input v-model="word" required placeholder="TodoListに入れたいものを入力" />
-    <button>追加</button>
-  </form>
+  <div>
+    <h1>TODO-APP</h1>
+    <div class="container">
+      <form @submit.prevent="pushList">
+        <input v-model="word" required placeholder="TodoListに入れたいものを入力" class="textbox" />
+        <button class="button">追加</button>
+      </form>
+    </div>
+  </div>
+
+  <div>
+    <div v-for="(todo, index) in todoList" :key="index" class="item">
+      <input
+        type="checkbox"
+        id="checkbox"
+        v-model="todo.isFinished"
+        style="transform: scale(1.5); margin-right: 10px"
+      />
+      <label
+        for="checkbox"
+        :class="{ deleteLine: todo.isFinished }"
+        style="vertical-align: middle"
+        >{{ todo.content }}</label
+      >
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+}
+.textbox {
+  font-size: 20px;
+  width: 300px;
+  height: 30px;
+}
+.button {
+  font-size: 15px;
+  width: 60px;
+  height: 36px;
+}
+.deleteLine {
+  text-decoration: line-through;
+}
+.item {
+  font-size: 30px;
+}
+</style>
